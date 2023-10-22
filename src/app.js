32d0235ea1ad0d7453940e06770e148db4ec0fd7
holly-fort2022 <holly.fort2022@gmail.com>
2023-10-22 11:00:39 -0400

    Created a city variable for the api URL

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
@@ -37,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "f8e1f05061a31a56175b1ffd3cbf1003";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
