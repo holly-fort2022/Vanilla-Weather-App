3a0ae0690e28aa1707951031a3aa8abbf36a50bd
holly-fort2022 <holly.fort2022@gmail.com>
2023-10-25 14:40:08 -0400

    Removed the content of weather forecast div into the JS code. Created a loop to add several days of forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="" alt="" id="icon" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">18º</span>
+          <span class="weather-forecast-temperature-min">14º</span>
+        </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -83,3 +105,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
